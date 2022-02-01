1afe2ee94ca5221163d841eedd53758308d1ff00
vlad-lab319 <solo15mag@gmail.com>
2022-01-31 20:58:42 -0500

    Add agregated data for all stores. exercise_5 complete

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Total revenue for the entire company: #{Store.sum(:annual_revenue)}"
+puts "Average annual revenue for all stores: #{Store.average(:annual_revenue)}"
+puts "Number of stores with $1M and more annual revenue: #{Store.where(["annual_revenue > ?",  1000000]).size}"
