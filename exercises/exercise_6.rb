1e9d666e1f1d2dc3a49b432420116342a93a1c1a
vlad-lab319 <solo15mag@gmail.com>
2022-01-31 23:36:54 -0500

    Add data validation. exercise_7 complete

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -10,14 +10,6 @@ puts "----------"
 
 # Your code goes here ...
 
-
-# create_table :employees do |table|
-#   table.references :store
-#   table.column :first_name, :string
-#   table.column :last_name, :string
-#   table.column :hourly_rate, :integer
-#   table.timestamps null: false
-
 class Store
   has_many :employees
 end
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,31 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store, absence: false
+end
+
+class Store
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+  validate :has_collection
+
+  def has_collection
+    if !mens_apparel && !womens_apparel 
+      errors.add(:collection, "Add at least one collection ") 
+    end
+  end
+end
+
+puts "Enter a store name: "
+store_name = gets.chomp
+new_store = Store.create(name: store_name)
+
+puts "Error creating '#{store_name}' because"
+  new_store.errors.each{ |error, message|
+    puts "#{error} #{message}"
+  }
